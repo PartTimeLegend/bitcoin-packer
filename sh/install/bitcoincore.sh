d9132b03211bb317d1df1205946b16c1f60a8599
Bailey <support@antonybailey.net>
2017-07-07 11:39:27 +0100

    Init

---
+++ sh/install/bitcoincore.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+wget https://bitcoin.org/bin/bitcoin-core-0.14.2/bitcoin-0.14.2-x86_64-linux-gnu.tar.gz
+tar -xvzf bitcoin-0.14.2-x86_64-linux-gnu.tar.gz
+cd bitcoin-0.14.2
+cd bin
+ls
---
+++ sh/install/yum-dependancies.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo yum install -y gcc-c++ libtool make autoconf automake openssl-devel libevent-devel boost-devel libdb4-devel libdb4-cxx-devel wget
